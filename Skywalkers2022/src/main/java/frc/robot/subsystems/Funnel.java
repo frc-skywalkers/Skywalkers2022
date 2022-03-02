ffb2d338e0e444e622685d2acfc120c83a414dfd
Chakshan Kothakota <ragchak@gmail.com>
2022-03-01 23:50:55 -0800

    funnel base subsystem

--- Skywalkers2022/src/main/java/frc/robot/subsystems/Funnel.java
+++ Skywalkers2022/src/main/java/frc/robot/subsystems/Funnel.java
@@ -4,14 +4,32 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.FunnelConstants;
 
 public class Funnel extends SubsystemBase {
   /** Creates a new Funnel. */
-  public Funnel() {}
+  CANSparkMax funnelMotor = new CANSparkMax(FunnelConstants.kMotorPort, MotorType.kBrushless);
+
+  public Funnel() {
+    funnelMotor.restoreFactoryDefaults();
+    funnelMotor.setInverted(FunnelConstants.kInvert);
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setOutput(double speed) {
+    funnelMotor.set(speed);
+  }
+
+  public void stop() {
+    setOutput(0);
+  }
+
 }
