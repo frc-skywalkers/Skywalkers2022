6b4f4aee18a93d7f6de475a9de8ed22dbf33d059
Chakshan Kothakota <ragchak@gmail.com>
2022-03-01 23:58:01 -0800

    indexer outline

--- Skywalkers2022/src/main/java/frc/robot/subsystems/Indexer.java
+++ Skywalkers2022/src/main/java/frc/robot/subsystems/Indexer.java
@@ -4,14 +4,45 @@
 
 package frc.robot.subsystems;
 
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants.IndexerConstants;
 
 public class Indexer extends SubsystemBase {
   /** Creates a new Indexer. */
-  public Indexer() {}
+
+  CANSparkMax indexerMotor = new CANSparkMax(IndexerConstants.kMotorPort, MotorType.kBrushless);
+  // implement color sensor
+  // implement beam breaker sensors
+
+  public Indexer() {
+
+    indexerMotor.restoreFactoryDefaults();
+    indexerMotor.setInverted(IndexerConstants.kInvert);
+
+  }
 
   @Override
   public void periodic() {
     // This method will be called once per scheduler run
   }
+
+  public void setOutput(double speed) {
+    indexerMotor.set(speed);
+  }
+
+  public void getColor() {
+    // return color that color sensor detects
+  }
+
+  public void isBallAtEntry() {
+    // return true if bottom beam breaker is broken
+  }
+  
+  public void isBallAtExit() {
+    // return true if top beam breaker is broken
+  }
+
 }
