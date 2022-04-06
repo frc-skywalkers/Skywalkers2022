c32f70200e45390425353c06e8b1cd1cb6f9711f
= <ansh.goodboy@gmail.com>
2022-04-06 16:39:42 -0700

    index ball fix

--- Skywalkers2022/src/main/java/frc/robot/commands/IndexBall.java
+++ Skywalkers2022/src/main/java/frc/robot/commands/IndexBall.java
@@ -25,13 +25,7 @@ public class IndexBall extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    ballToBeIndexed = !indexer.isBallAtExit();
-
-    if (ballToBeIndexed) {
-      indexer.on();
-    } else {
-      indexer.off();
-    }
+    indexer.on();
   }
 
   // Called once the command ends or is interrupted.
@@ -43,6 +37,6 @@ public class IndexBall extends CommandBase {
   // Returns true when the command should end.
   @Override
   public boolean isFinished() {
-    return false;
+    return indexer.isBallAtExit();
   }
 }
